cdb5e704c1dbe40188b36c26bfd729c662bc4e49
Vitaly Tolmachev <vitalhare@outlook.com>
2023-10-16 23:07:54 +0500

    Task7

---
+++ Tyuiu.TolmachevVV.Sprint2.Task7.V12.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.TolmachevVV.Sprint2.Task7.V12.Lib
+{
+    public class DataService : ISprint2Task7V12
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            if (x < -2)
+            {
+                if (y > 4) { return true; }
+                else return false;
+            }
+            else if (x > 0 && x < 1)
+            {
+                if (y > 0 && y < 1) { return true; }
+                else return false;
+            }
+            else return false;
+        }
+    }
+}
---
+++ Tyuiu.TolmachevVV.Sprint2.Task7.V12.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.TolmachevVV.Sprint2.Task7.V12.Lib;
+
+namespace Tyuiu.TolmachevVV.Sprint2.Task7.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckDotInShadedAreaCheck()
+        {
+            DataService service1 = new DataService();
+            Assert.IsTrue(service1.CheckDotInShadedArea(-3, 5));
+        }
+    }
+}
